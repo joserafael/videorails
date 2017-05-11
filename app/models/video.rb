db854e48b439c0a38ce8c505f4350466eec34fb1
Jose Rafael Camejo <webmaster@joserafael.com.ve>
2017-05-10 22:21:07 -0400

    change resolution thumb

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,10 +4,10 @@ class Video < ApplicationRecord
 
 	has_attached_file :archivovideo, :styles => {
     :medium => { :geometry => "640x480", :format => 'mp4' },
-    :cuadro1 => { :geometry => "100x100#", :format => 'jpg', :time => 1 },
-    :cuadro2 => { :geometry => "100x100#", :format => 'jpg', :time => 30 },
-    :cuadro3 => { :geometry => "100x100#", :format => 'jpg', :time => 50 },
-    :cuadro4 => { :geometry => "100x100#", :format => 'jpg', :time => 100 }
+    :cuadro1 => { :geometry => "640x480#", :format => 'jpg', :time => 1 },
+    :cuadro2 => { :geometry => "640x480#", :format => 'jpg', :time => 30 },
+    :cuadro3 => { :geometry => "640x480#", :format => 'jpg', :time => 50 },
+    :cuadro4 => { :geometry => "640x480#", :format => 'jpg', :time => 100 }
   }, :processors => [:transcoder]
 
 	do_not_validate_attachment_file_type :archivovideo
